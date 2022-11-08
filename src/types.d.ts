9163f9054d8f900546dc80e7e2418f49db1aea6c
omrsfylmz <omrsfylmz@gmail.com>
2022-11-08 19:25:39 +0300

    Add pokemon types

---
+++ src/types.d.ts
@@ -0,0 +1,9 @@
+export type Pokemon = {
+  image: string;
+  id: number;
+  name: string;
+  types: [{ type: { name: string } }];
+  weight: number;
+  height: number;
+  stats: [{ base_stat: number; stat: { name: string } }];
+};
